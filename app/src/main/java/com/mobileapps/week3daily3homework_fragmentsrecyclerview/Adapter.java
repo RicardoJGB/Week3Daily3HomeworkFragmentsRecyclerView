9d2e525ead5751524125f742589df29c1c5f65df
Ricardo Bravo <rjesuag@gmail.com>
2018-11-30 13:45:20 -0500

    Added Display Fragment and Adapter Class. Started working on Recycler View.

--- app/src/main/java/com/mobileapps/week3daily3homework_fragmentsrecyclerview/Adapter.java
+++ app/src/main/java/com/mobileapps/week3daily3homework_fragmentsrecyclerview/Adapter.java
@@ -1,31 +1,52 @@
 package com.mobileapps.week3daily3homework_fragmentsrecyclerview;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    ArrayList<String> receivedList = new ArrayList<>();
+    Context context;
+
+    public Adapter (ArrayList<String> receivedList, Context context){
+        this.receivedList=receivedList;
+        this.context=context;
+    }
 
     @NonNull
     @Override
     public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view= LayoutInflater.from(context).inflate(R.layout.data_received_item,viewGroup,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Adapter.ViewHolder viewHolder, int i) {
+        viewHolder.tvItemData.setText(receivedList.get(i));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return receivedList.size();
+    }
+
+    public void getNewInput(String nextInput){
+        receivedList.add(nextInput);
+        notifyDataSetChanged();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvItemData;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvItemData=itemView.findViewById(R.id.tvReceivedData);
         }
     }
 }
--- app/src/main/java/com/mobileapps/week3daily3homework_fragmentsrecyclerview/Fragments/DisplayFragment.java
+++ app/src/main/java/com/mobileapps/week3daily3homework_fragmentsrecyclerview/Fragments/DisplayFragment.java
@@ -3,13 +3,17 @@ package com.mobileapps.week3daily3homework_fragmentsrecyclerview.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.mobileapps.week3daily3homework_fragmentsrecyclerview.Adapter;
 import com.mobileapps.week3daily3homework_fragmentsrecyclerview.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +22,14 @@ public class DisplayFragment extends Fragment {
     public static final String RCV_TAG="receiving data";
     TextView tvUserInput;
     RecyclerView rvDataReceived;
-    
+    Adapter adapter;
+
+    ArrayList<String> receivedDataList=new ArrayList<>();
+
+    public static DisplayFragment newInstance(){
+        DisplayFragment displayFragment = new DisplayFragment();
+        return displayFragment;
+    }
 
 
     public DisplayFragment() {
@@ -29,8 +40,17 @@ public class DisplayFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display, container, false);
+        View view = inflater.inflate(R.layout.fragment_display,container,false);
+        receivedDataList.add("MESSAGE");
+        rvDataReceived=view.findViewById(R.id.rvDataReceived);
+        adapter=new Adapter(receivedDataList,this.getContext());
+        rvDataReceived.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        rvDataReceived.setAdapter(adapter);
+        return view;
+    }
+
+    public void receiveData (String msgReceived){
+        adapter.getNewInput(msgReceived);
     }
 
 }
